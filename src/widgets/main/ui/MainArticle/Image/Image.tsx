32bbdf36360cfc19d55f316908555d26be504b4c
Oleg Kuzmin <info@oleg-kuzmin.ru>
2025-08-28 16:48:33 +0300

    feat: add srcSet to MainArticle - Image

--- src/widgets/main/ui/MainArticle/Image/Image.tsx
+++ src/widgets/main/ui/MainArticle/Image/Image.tsx
@@ -6,5 +6,21 @@ interface ImageProps {
 }
 
 export function Image({ className }: Readonly<ImageProps>) {
-  return <img className={cn(styles.image, className)} src="/kolibri-layout-site/images/vitalik.png" alt="Vitalik Buterin"></img>;
+  return (
+    <img
+      className={cn(styles.image, className)}
+      src="/kolibri-layout-site/images/vitalik.png"
+      alt="Vitalik Buterin"
+      srcSet="
+        /kolibri-layout-site/images/vitalik-mobile_1x.png 345w, 
+        /kolibri-layout-site/images/vitalik-mobile_2x.png 690w, 
+        /kolibri-layout-site/images/vitalik-mobile_3x.png 1035w, 
+        /kolibri-layout-site/images/vitalik-desktop_1x.png 625w,
+        /kolibri-layout-site/images/vitalik-desktop_2x.png 1250w,
+        /kolibri-layout-site/images/vitalik-desktop_3x.png 1875w"
+      sizes="
+        (width <= 768px) 345px,
+        625px"
+    />
+  );
 }
